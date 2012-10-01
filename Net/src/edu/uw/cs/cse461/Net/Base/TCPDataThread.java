fe72c802896df17ca57e7ba871da14e433931eea
mmattb <mmattb@gmx.com>
2012-10-01 11:22:19 -0700

    Added voluminous comments to TCPDataThread.java.  Whatev.

--- Net/src/edu/uw/cs/cse461/Net/Base/TCPDataThread.java
+++ Net/src/edu/uw/cs/cse461/Net/Base/TCPDataThread.java
@@ -7,22 +7,47 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 
+/*************
+ * Runnable server thread for a single socket.
+ * Waits for a TCP connection to be established, then sends a packet of a given size.
+ * Makes no guarantees what the packet's contents will be.
+ * Call end() to signal the thread to shut down; this may take up to TIMEOUT ms to occur. 
+ * @author mmattb
+ *
+ */
 public class TCPDataThread implements Runnable{
-	private final static int TIMEOUT = 750;
-	private int _portNumber;
-	private ServerSocket _server;
-	private boolean _timeToClose = false;
-	private int _xferSize;
+	private final static int TIMEOUT = 750; //time in MS between checks to see if its time to shut down
+	private int _portNumber;					//port number to receive connections in
+	private ServerSocket _server;				//a server socket for the current available connection
+	private boolean _timeToClose = false;		//flag set when end() is called to signal the thread to shut down
+	private int _xferSize;						//size of server response in bytes
 	
+	/*************
+	 * Constructs a new thread, but does not start it running
+	 * @param portNumber - a valid network port number in this domain
+	 * @param xferSize - the size in bytes of the response to be sent
+	 */
 	public TCPDataThread(int portNumber, int xferSize){
 		_portNumber = portNumber;
 		_xferSize = xferSize;
 		System.out.println("TCPDataThread constructor: server set up at port: " + portNumber);
 	}
 	
-	
+	/*************
+	 * Starts this thread running if called using the Java Thread functionality.
+	 * Returns prematurely if the socket cannot be set up properly.  This can happen
+	 *   for many reasons, including:
+	 *   -_portNumber is not available
+	 *   -an exception is thrown while the socket TIMEOUT is being set (perhaps a bad value)
+	 * Once the socket is made, it will wait for a connection.  If a connection is established,
+	 *   it will send a packet of size _xferSize bytes to the client.  There are no guarantees
+	 *   what the contents of the packet will be.
+	 * Every TIMEOUT ms, the thread will check the _timeToClose flag to see if its time to shut
+	 *   this server thread down.  The flag can be set to true by a call to end() from another thread.
+	 */
 	@Override
 	public void run() {
+		//attempt to set up socket on target port number
 		try {
 			_server = new ServerSocket(_portNumber);
 		} catch (IOException e) {
@@ -30,6 +55,7 @@ public class TCPDataThread implements Runnable{
 			return;
 		}
 		
+		//attempt to set the timeout between successive checks of _timeToClose flag
 		try {
 			_server.setSoTimeout(TIMEOUT);
 		} catch (SocketException e) {
@@ -39,10 +65,13 @@ public class TCPDataThread implements Runnable{
 		
 		System.out.println("TCPDataThread.run: server started at port: " + _portNumber);
 		
+		//loop until time to close
 		boolean error = false;
 		boolean timedOut = false;
 		while(true){
 			Socket s = null;
+			//set up socket to accept.  Code hangs on .accept() until a connection is established or
+			// TIMEOUT ms have passed.
 			try {
 				s = _server.accept();
 				System.out.println("TCPDataThread.run: TCP connection established.");
@@ -53,6 +82,8 @@ public class TCPDataThread implements Runnable{
 				error = true;
 			} 
 			
+			//if there was no connection error and the timeout did not expire, then we have made a connection
+			//  and we attempt to send the response
 			if(!error && !timedOut){
 				try {
 					System.out.println("TCPDataThread.run: transmitting message of length " + _xferSize + ".");
@@ -72,7 +103,7 @@ public class TCPDataThread implements Runnable{
 			} 
 			
 			
-			//check if time to close
+			//check if time to close; close if it is
 			if(_timeToClose){
 				try {
 					_server.close();
@@ -88,6 +119,10 @@ public class TCPDataThread implements Runnable{
 		
 	}
 	
+	/*************
+	 * Call this function from the parent process to end this server thread.
+	 * May take up to TIMEOUT ms until the thread checks this value and shuts down.
+	 */
 	public void end(){
 		_timeToClose = true;
 	}
