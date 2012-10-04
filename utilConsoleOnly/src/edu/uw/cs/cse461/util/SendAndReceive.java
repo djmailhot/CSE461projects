6237fc0d2692e341e5aaa4323744b5fafcd52170
David Mailhot <djmailhot@gmail.com>
2012-10-03 23:03:19 -0700

    Fixed up failed trial behavior to not be included in data throughput calculations

--- utilConsoleOnly/src/edu/uw/cs/cse461/util/SendAndReceive.java
+++ utilConsoleOnly/src/edu/uw/cs/cse461/util/SendAndReceive.java
@@ -14,47 +14,35 @@ public class SendAndReceive {
 	 * @param socket - the socket to send the packet from and receive a response from
 	 * @param packet - the packet to send through the socket to the server
 	 * @param len - the length of the total amount of data should be received
-	 * @return the number of bytes received through the connection, -1 if an error occurred before bytes were received.
+	 * @return the number of bytes received through the connection, -1 if an error occurred before bytes were sent.
 	 */
 	public static int udpSendPacket(DatagramSocket socket, DatagramPacket packet, int len) {
+		int received = 0;
 		try {
-			socket.send(packet);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return -1;
-		}
-		// Attempts to send the packet.
+			// Attempts to send the packet.
+			try {
+				socket.send(packet);
+			} catch (IOException e) {
+				// If the sending failed, return an error code
+				return -1;
+			}
 		
-		if (len >= 1000) {
 			// The length of data expected is larger than can be sent in a single packet.
 			// Thus an alternate array must be created to store the data until all of it arrives or
 			// the socket times out.
 			DatagramPacket response = new DatagramPacket(new byte[1000], 1000);
-			int received = 0;
-			try {
-				while (received < len) {
-					socket.receive(response);
-					
-					received += response.getLength();
-				}
-			} catch (SocketTimeoutException e) {
-				return received;
-				// If the socket times out, return what was received.
-			} catch (IOException e) {
-				e.printStackTrace();
-				return -1;
-			}
-			return received;
-		} else {
-			DatagramPacket response = new DatagramPacket(new byte[len], len);
-			try {
+			while (received < len) {
 				socket.receive(response);
-			} catch (IOException e) {
-				e.printStackTrace();
-				return -1;
+				received += response.getLength();
 			}
-			return response.getLength();
+		} catch (SocketTimeoutException e) {
+			e.printStackTrace();
+			// If the socket times out, return what was received.
+		} catch (IOException e) {
+			e.printStackTrace();
+			// If there is an error, return what was received, if anything.
 		}
+		return received;
 	}
 
 	/**
@@ -68,17 +56,13 @@ public class SendAndReceive {
 			InputStream result = socket.getInputStream();
 			OutputStream os = socket.getOutputStream();
 			os.write(buf);
-			try {
-				int len = 0;
-				while ( len < buf.length) {
-					len += result.read(buf, len, buf.length);
-				}
-				String response = new String(buf, 0, buf.length);
-				return response;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return null;
+			
+			int len = 0;
+			while ( len < buf.length) {
+				len += result.read(buf, len, buf.length);
 			}
+			String response = new String(buf, 0, buf.length);
+			return response;
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
