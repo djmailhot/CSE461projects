53c7e77575784afa9f7ed1e5d3390c1badf4db14
Lydia Duncan <lydiad2@uw.edu>
2012-10-18 11:08:49 -0700

    RPCCall works when tested against EchoRPC and the EchoRPCService run for the class, except the success message is different from what I expect.  I think this is JZ's problem, though, so I'm committing this.

--- Net/src/edu/uw/cs/cse461/Net/RPC/RPCCall.java
+++ Net/src/edu/uw/cs/cse461/Net/RPC/RPCCall.java
@@ -1,6 +1,7 @@
 package edu.uw.cs.cse461.Net.RPC;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Timer;
@@ -11,6 +12,7 @@ import org.json.JSONObject;
 
 import edu.uw.cs.cse461.Net.Base.NetBase;
 import edu.uw.cs.cse461.Net.Base.NetLoadable.NetLoadableService;
+import edu.uw.cs.cse461.Net.TCPMessageHandler.TCPMessageHandler;
 import edu.uw.cs.cse461.util.Log;
 
 /**
@@ -89,7 +91,70 @@ public class RPCCall extends NetLoadableService {
 			JSONObject userRequest,   // arguments to send to remote method
 			boolean tryAgain          // true if an invocation failure on a persistent connection should cause a re-try of the call, false to give up
 			) throws JSONException, IOException {
-		return null;
+		Socket socket = new Socket(ip, port);
+		TCPMessageHandler handler = new TCPMessageHandler(socket);
+		JSONObject handshake = new JSONObject();
+		int callid = 5; // This choice is arbitrary TODO figure out a better id
+		handshake.put("id", callid); 
+		handshake.put("host", ip);
+		handshake.put("action", "connect");
+		handshake.put("type", "control");
+		
+		// If a persistent connection is possible, this is where code would be added 
+		// Extra credit TODO
+		
+		Log.d(TAG, "Sending handshake to service");
+		handler.sendMessage(handshake);
+		
+		Log.d(TAG, "Attempting to receive response to handshake");
+		JSONObject response = handler.readMessageAsJSONObject();
+		
+		
+		String result = response.getString("type");
+		// If an error occurs, we want to be informed of it and stop executing this message.
+		if (result.equals("ERROR")) {
+			return response;
+		} else if (result.equals("OK")) {
+			// Makes sure that the response we are receiving was actually for the handshake we sent
+			if (response.getInt("callid") != callid) {
+				Log.e(TAG, "Received a response from the server with the wrong callid number");
+				Log.e(TAG, response.toString());
+				return response;
+			}
+			Log.d(TAG, "Handshake successful");
+		}
+		
+		// If we have reached this point, we know that the handshake was successful, so it is safe to send the user's message.
+		JSONObject call = new JSONObject();
+		call.put("type", "invoke");
+		call.put("id", callid*2);
+		call.put("app", serviceName);
+		call.put("method", method);
+		call.put("host", ip);
+		JSONObject message = new JSONObject();
+		message.put("msg", userRequest);
+		call.put("args", message);
+		
+		Log.d(TAG, "Attempting to send the call");
+		handler.sendMessage(call);
+		
+		Log.d(TAG, "Attempting to receive a response");
+		response = handler.readMessageAsJSONObject();
+		// Makes sure that the response we are receiving was actually for the call we sent
+		if (response.getInt("callid") != callid*2) {
+			Log.e(TAG, "Received a response to the call from the server with the wrong callid number");		
+			return null;
+		} else {
+			result = response.getString("type");
+			if (result.equals("ERROR")) {
+				Log.e(TAG, "Error received: " + response.get("message").toString());
+				Log.e(TAG, "on message " + response.getJSONObject("callargs").toString());
+				return null;
+			} else {
+				Log.d(TAG, response.toString());
+				return response.getJSONObject("value");
+			}
+		}
 	}
 	
 	/**
